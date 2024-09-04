a86b37b7df2ea21d2d86605adf9f362a50333399
Chris.Chen <Chris.Chen@splashtop.com>
2024-09-04 18:44:30 +0800

    footer finish

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('page/', include('myapp.urls')),
-    path('', TemplateView.as_view(template_name='index.html'), name='template_view')
+    path('', TemplateView.as_view(template_name='index.html'), name='template_view'),
+    path("index2/", TemplateView.as_view(template_name="index2.html"),name="index2"),
 ]
